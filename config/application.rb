a510f803e75eaf9708540e5f5e10bf8fc18069b4
tetsu0804 <banginhoodyonezawa@gmail.com>
2021-10-08 22:47:57 +0900

    RSpecインストール完了 Userモデル emailインデックス付与完了

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,12 @@ module CocoShoppingList
     config.generators do |g|
       g.assets = false
       g.helper = false
+      g.test_framework :rspec,
+            view_specs: false,
+            routing_specs: false,
+            helper_specs: false,
+            controller_specs: false,
+            request_specs: false
     end
   end
 end
