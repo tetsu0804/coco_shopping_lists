462d89a9396dfaa1150d719d8ee7874e79085c75
tetsu0804 <banginhoodyonezawa@gmail.com>
2021-10-09 22:47:25 +0900

    axiosとrails-ujsインストール と VueAxiosPluginに追加 to packs/application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from '../app.vue'
 import router from '../router/index'
+import VueAxiosPlugin from '../Plugin/VueAxios'
+import axios from 'axios'
+import { csrfToken } from 'rails-ujs'
+
+Vue.use(VueAxiosPlugin, { axios: axios, csrfToken: csrfToken });
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
