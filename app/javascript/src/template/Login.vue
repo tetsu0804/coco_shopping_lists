79d8b0ddeecd14e66c4f37a89002725fba302d32
tetsu0804 <banginhoodyonezawa@gmail.com>
2021-10-10 17:03:51 +0900

    LoginForm.vueを子コンポーネントに追加 to Login.vue

---
+++ app/javascript/src/template/Login.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <router-link :to="{ name: 'Home' }">home</router-link>
+    <router-link :to="{ name: 'Signup' }">ユーザー登録</router-link>
+    <div>
+      getters( 'userLoggedIn '): {{ userLoggedIn }}
+    </div>
+    <h1>ログイン</h1>
+    <login-form></login-form>
+  </div>
+</template>
+
+<script>
+  import LoginForm from '../molcules/LoginForm.vue'
+  import { mapGetters } from 'vuex'
+  export default {
+    components: {
+      LoginForm
+    },
+    computed: mapGetters(['userLoggedIn'])
+  }
+</script>
+
+<style scoped>
+  h1 {
+    text-align: center;
+  }
+</style>
