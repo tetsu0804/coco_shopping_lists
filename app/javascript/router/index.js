2ff663dea0d1a51485d2d904499ed53622281e5e
tetsu0804 <banginhoodyonezawa@gmail.com>
2021-10-10 17:03:13 +0900

    Login追加 to router/index.js

--- app/javascript/router/index.js
+++ app/javascript/router/index.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../src/template/Home.vue'
 import Signup from '../src/template/Signup.vue'
+import Login from '../src/template/Login.vue'
 Vue.use(VueRouter)
 
 export default new VueRouter({
   routes: [
     { path: '/', component: Home, name: 'Home' },
     { path: '/signup', component: Signup, name: 'Signup' },
+    { path: '/login', component: Login, name: 'Login' },
   ]
 });
