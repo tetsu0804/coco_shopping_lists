914f673f5db63485516f5001cad7998a6ec099f7
tetsu0804 <banginhoodyonezawa@gmail.com>
2021-10-10 16:59:24 +0900

    router-link ログイン 追加 to Signup.vue

--- app/javascript/src/template/Signup.vue
+++ app/javascript/src/template/Signup.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
     <router-link :to="{ name: 'Home' }">home</router-link>
+    <router-link :to="{ name: 'Login' }">ログイン</router-link>
+    <div>
+      getters( 'userLoggedIn '): {{ userLoggedIn }}
+    </div>
     <h1>ユーザー登録</h1>
     <signup-form></signup-form>
   </div>
@@ -8,10 +12,12 @@
 
 <script>
   import SignupForm from '../molcules/SignupForm.vue'
+  import { mapGetters } from 'vuex'
   export default {
     components: {
       SignupForm
-    }
+    },
+    computed: mapGetters(['userLoggedIn'])
   }
 </script>
 
