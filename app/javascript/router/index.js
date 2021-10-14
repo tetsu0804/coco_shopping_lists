8c1fa46192b7ad9f13ecec7c59755e577c9c27cc
tetsu0804 <banginhoodyonezawa@gmail.com>
2021-10-14 21:24:02 +0900

    router/index.js で import CategoryCreate.vue

--- app/javascript/router/index.js
+++ app/javascript/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Home from '../src/template/Home.vue'
 import Signup from '../src/template/Signup.vue'
 import Login from '../src/template/Login.vue'
+import CategoryCreate from '../src/template/CategoryCreate.vue'
 import { authorizeLogin } from './gurds'
 Vue.use(VueRouter)
 
@@ -11,6 +12,7 @@ const router = new VueRouter({
     { path: '/', component: Home, name: 'Home', meta: { requiresAuth: true } },
     { path: '/signup', component: Signup, name: 'Signup' },
     { path: '/login', component: Login, name: 'Login' },
+    { path: '/categories', component: CategoryCreate, name: 'CategoryCreate', meta: { requiresAuth: true } }
   ]
 });
 
