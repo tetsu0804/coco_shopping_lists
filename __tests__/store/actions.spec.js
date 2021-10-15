eec2fbec5a29d0007ada61c39d8a10ffb77a974d
tetsu0804 <banginhoodyonezawa@gmail.com>
2021-10-15 20:38:30 +0900

    actions.spec.js で fetchCreateCategory 追加

--- __tests__/store/actions.spec.js
+++ __tests__/store/actions.spec.js
@@ -1,7 +1,7 @@
 import actions from '@/store/actions'
 
 describe('/store/actioins', () => {
-  let commit, user
+  let commit, user, category, categories
   beforeEach(() => {
     commit = jest.fn();
     user = { id: 1, first_name: '太朗', last_name: '吉田', email: 'test@test.com'}
@@ -27,6 +27,24 @@ describe('/store/actioins', () => {
     it('commitの引数が logoutUser になっているか?', () => {
       actions.fetchLogoutUser( { commit });
       expect(commit).toHaveBeenCalledWith('logoutUser');
+    });
+  });
+
+  describe('fetchCreateCategory', () => {
+    it('( 一つのカテゴリ )commitの引数が createCategory と category になっているか？', () => {
+      category = { id: 1, category_name: 'ご飯' }
+      actions.fetchCreateCategory( { commit }, category);
+      expect(commit).toHaveBeenCalledWith('createCategory', category);
+    });
+
+    it('( 複数のカテゴリ ) commitの引数が createCategory と categories になっているか？', () => {
+      categories = [
+        { id: 1, category_name: 'ご飯' },
+        { id: 2, category_name: 'おやつ' },
+        { id: 3, category_name: 'しつけ' }
+      ]
+      actions.fetchCreateCategory( { commit }, categories);
+      expect(commit).toHaveBeenCalledWith('createCategory', categories);
     });
   });
 });
