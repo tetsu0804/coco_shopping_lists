b42e7817a3d6e3edd805886f1c37049d6d23917c
tetsu0804 <banginhoodyonezawa@gmail.com>
2021-10-15 20:43:08 +0900

    CategoryCreate.vue import Categoryorm.vue と Flash.veu 追加

---
+++ app/javascript/src/template/CategoryCreate.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <router-link :to="{ name: 'Home' }">Home</router-link>
+    category
+    <flash
+      :flash="flash"
+      @closeFlash="flash = { message: '', status: '' }"
+    ></flash>
+    <category-form
+      @categoryStatus="flash = $event"
+    ></category-form>
+    <div v-for="category in allCategories" :key="category.id">
+      {{ category.category_name }}
+    </div>
+  </div>
+</template>
+
+<script>
+  import CategoryForm from '../molcules/CategoryForm.vue'
+  import Flash from '../atoms/Flash.vue'
+  import { mapGetters } from 'vuex'
+  export default {
+    components: {
+      CategoryForm,
+      Flash
+    },
+    data() {
+      return {
+        flash: { message: '', status: '' },
+      }
+    },
+    computed: mapGetters(['allCategories'])
+  }
+</script>
+
+<style scoped>
+  .non-category-msg {
+    border: 1px solid rgb(92, 21, 21);
+  }
+  .category-ids {
+    border-bottom: 1px solid rgb(0, 0, 0);
+  }
+</style>
