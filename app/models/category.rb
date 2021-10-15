e05c08c55d753478436ac2483576ec01ed6b0c7a
tetsu0804 <banginhoodyonezawa@gmail.com>
2021-10-15 20:37:18 +0900

    category.rb で select_colmn 追加

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,7 @@
 class Category < ApplicationRecord
   validates :category_name, presence: true, length: { maximum: 20 }, uniqueness: true
+
+  def select_column
+    return { id: self.id, category_name: self.category_name }
+  end
 end
