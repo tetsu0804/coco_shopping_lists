f273ce5372174138b139450b5f688dcab4d3e159
tetsu0804 <banginhoodyonezawa@gmail.com>
2021-10-17 20:43:52 +0900

    Practice.vue 追加

---
+++ app/javascript/src/molcules/Practice.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div v-for="category  in categories" :key="category.id">
+      {{ category.category_name}}
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  computed: {
+    categories() {
+      return this.$store.getters.allCategories;
+    },
+  },
+  watch: {
+    categories: function(newValue, oldValue) {
+      debugger
+    }
+  }
+}
+</script>
