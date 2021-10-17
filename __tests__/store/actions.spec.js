e9a83c6115acde47bd4957d0c66bfb67ed206a01
tetsu0804 <banginhoodyonezawa@gmail.com>
2021-10-17 20:35:38 +0900

    actions.spec.js で category > create と update と destroy 追加

--- __tests__/store/actions.spec.js
+++ __tests__/store/actions.spec.js
@@ -1,7 +1,7 @@
 import actions from '@/store/actions'
 
 describe('/store/actioins', () => {
-  let commit, user, category, categories
+  let commit, user
   beforeEach(() => {
     commit = jest.fn();
     user = { id: 1, first_name: '太朗', last_name: '吉田', email: 'test@test.com'}
@@ -30,21 +30,53 @@ describe('/store/actioins', () => {
     });
   });
 
-  describe('fetchCreateCategory', () => {
-    it('( 一つのカテゴリ )commitの引数が createCategory と category になっているか？', () => {
+  describe('カテゴリ ( category )', () => {
+    let category, categories, update_category, delete_num
+    beforeEach(() => {
       category = { id: 1, category_name: 'ご飯' }
-      actions.fetchCreateCategory( { commit }, category);
-      expect(commit).toHaveBeenCalledWith('createCategory', category);
-    });
-
-    it('( 複数のカテゴリ ) commitの引数が createCategory と categories になっているか？', () => {
       categories = [
         { id: 1, category_name: 'ご飯' },
         { id: 2, category_name: 'おやつ' },
         { id: 3, category_name: 'しつけ' }
       ]
-      actions.fetchCreateCategory( { commit }, categories);
-      expect(commit).toHaveBeenCalledWith('createCategory', categories);
+    });
+
+    describe('fetchCreateCategory', () => {
+      it('( 一つのカテゴリ )commitの引数が createCategory と category になっているか？', () => {
+        actions.fetchCreateCategory( { commit }, category);
+        expect(commit).toHaveBeenCalledWith('createCategory', category);
+      });
+    });
+
+    describe('fetchCreateAllCategories', () => {
+      it('( 複数のカテゴリ ) commitの引数が createAllCategories と categories になっているか？', () => {
+        actions.fetchCreateAllCategories( { commit }, categories);
+        expect(commit).toHaveBeenCalledWith('createAllCategories', categories);
+      });
+    });
+
+    describe('fetchUpdateCategory', () => {
+      it('commit の引数が updateCategory と update_categoryになっているか？', () => {
+        update_category = { id: 1, category_name: 'パン' }
+        actions.fetchUpdateCategory({ commit }, update_category);
+        expect(commit).toHaveBeenCalledWith('updateCategory', update_category);
+      });
+    });
+
+    describe('fetchAllDeleteCategory', () => {
+      it('全てのカテゴリを削除した時の commit の引数が allDeleteCategory と categories になっているか？', () => {
+        actions.fetchAllDeleteCategory( { commit });
+        expect(commit).toHaveBeenCalledWith('allDeleteCategory');
+      });
+    });
+
+    describe('fetchDeleteCategory', () => {
+      it('commit の引数が deleteCategory と id になっているか？', () => {
+        delete_num = 2
+        actions.fetchDeleteCategory( { commit }, delete_num);
+        expect(commit).toHaveBeenCalledWith('deleteCategory', 2);
+      });
     });
   });
+
 });
