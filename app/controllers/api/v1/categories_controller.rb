7f00fdf45f50ec817367a1da8ac3e165221151af
tetsu0804 <banginhoodyonezawa@gmail.com>
2021-10-17 20:41:41 +0900

    categories_controller.rb で create と update と　destroy 追加

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -10,11 +10,31 @@ class Api::V1::CategoriesController < ApplicationController
   end
 
   def create
-    category = Category.new(category_name: params[:category_name])
+    user = User.find(params[:user_id])
+    category = user.categories.build(category_name: params[:category_name])
+
     if category.save
       render json: { category: category}, status: 200
     else
       render json: { message: 'カテゴリ作成失敗しました。'}, status: 400
     end
+  end
+
+  def update
+    category = Category.find(params[:id])
+    if !category.nil?
+      if category.update_attribute(:category_name, params[:category_name])
+        render json: { category: category }, status: 200
+      else
+        render json: { message: '編集失敗しました。もう一度入力してください。'},status: 400
+      end
+    else
+      render json: { message: '編集できませんでした。もう一度入力ください。'}, status: 400
+    end
+  end
+
+  def destroy
+    Category.find(params[:id]).destroy
+    head :no_content
   end
 end
