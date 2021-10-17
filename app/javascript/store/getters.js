d9da4e8980158d339c9fb2882cbe0ed937c309c2
tetsu0804 <banginhoodyonezawa@gmail.com>
2021-10-17 20:37:01 +0900

    getters.js で　selectedCategory 追加

--- app/javascript/store/getters.js
+++ app/javascript/store/getters.js
@@ -6,6 +6,23 @@ export default {
     return state.loggedIn
   },
   allCategories(state) {
-    return state.categories
+    if (state.categories.length > 0) {
+      return state.categories
+    } else {
+      return [{ id: '' , category_name: '登録なし' }]
+    }
+  },
+  selectedCategory(state) {
+    return (category_id) => {
+      let selected = []
+      if (!!state.categories) {
+        for(let i = 0; i < state.categories.length; i++) {
+          if (state.categories[i].id === category_id) {
+            selected.push(state.categories[i])
+          }
+        }
+      }
+      return !!selected.length ? selected[0] : {id: '', category_name: '登録なし', user_id: ''}
+    }
   }
 }
