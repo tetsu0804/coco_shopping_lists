49a988968030f0b0fe8ffcbaa2be50f4455cdc1d
tetsu0804 <banginhoodyonezawa@gmail.com>
2021-10-19 20:44:45 +0900

    rails g model ShopList list_name:string price:integer purchasedate:datetime user:references コマンド実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_15_130518) do
+ActiveRecord::Schema.define(version: 2021_10_19_110959) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
     t.string "category_name", null: false
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 2021_10_15_130518) do
     t.bigint "user_id"
     t.index ["category_name"], name: "index_categories_on_category_name", unique: true
     t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "shop_lists", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
+    t.string "list_name", null: false
+    t.integer "price", null: false
+    t.datetime "purchasedate", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_shop_lists_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
@@ -33,4 +43,5 @@ ActiveRecord::Schema.define(version: 2021_10_15_130518) do
   end
 
   add_foreign_key "categories", "users"
+  add_foreign_key "shop_lists", "users"
 end
