31e1d0a65c9c6c50cfbc60878c3f24f56e7c5282
tetsu0804 <banginhoodyonezawa@gmail.com>
2021-10-26 23:33:59 +0900

    getters.js で thisMonthShopList と mainDisplay 追加

--- app/javascript/store/getters.js
+++ app/javascript/store/getters.js
@@ -1,3 +1,6 @@
+import date from '../src/modules/date'
+import shoplist from '../src/modules/shoplist'
+
 export default {
   allUsers(state) {
     return state.users
@@ -27,5 +30,39 @@ export default {
   },
   allShoplists(state) {
     return state.shoplists
+  },
+  // ◯年◯月のsort したstate.shoplists
+  thisMonthShopList(state) {
+    return (date_num) => {
+      let this_month, filter_shoplists, regexp_month, sort_shoplists
+
+      if (state.shoplists.length > 0) {
+        this_month = date.getThisMonth(date_num);
+        regexp_month = date.regexpYearMonth(this_month)
+        filter_shoplists = shoplist.filteringMonthShopList(state.shoplists, regexp_month);
+        sort_shoplists = filter_shoplists.length > 0 ? shoplist.descendingOrderShopLists(filter_shoplists): []
+      } else {
+        sort_shoplists = []
+      }
+      return sort_shoplists
+    }
+  },
+  mainDisplay(state, getters) {
+    return (date_num) => {
+      let now = new Date(), total_price = 0, format_display = {last_shoplist: '', total_price: '', month_display: ''}, date_num_month_shoplists, this_month, now_judge, split_date, month_display, last_shoplist
+
+      date_num_month_shoplists = getters.thisMonthShopList(date_num);
+
+      format_display.total_price = shoplist.thisMonthTotalPrice(date_num_month_shoplists);
+
+      format_display.last_shoplist = shoplist.thisMonthLastShopList(date_num_month_shoplists);
+
+      this_month = date.getThisMonth(date_num);
+      now_judge = date.regexpYearMonth(now)
+
+      format_display.month_display = shoplist.thisMonthDisplayTitle(this_month, now_judge);
+
+      return format_display
+    }
   }
 }
