19c9e8f2701871d04d4dc2d97b0d5cd1e33f7020
tetsu0804 <banginhoodyonezawa@gmail.com>
2021-10-29 10:12:33 +0900

    router/index.js で import MonthDetail 追加

--- app/javascript/router/index.js
+++ app/javascript/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Home from '../src/template/Home.vue'
 import Signup from '../src/template/Signup.vue'
 import Login from '../src/template/Login.vue'
+import MonthDetail from '../src/template/MonthDetail.vue'
 import CategoryCreate from '../src/template/CategoryCreate.vue'
 import { authorizeLogin } from './gurds'
 Vue.use(VueRouter)
@@ -12,7 +13,8 @@ const router = new VueRouter({
     { path: '/', component: Home, name: 'Home', meta: { requiresAuth: true } },
     { path: '/signup', component: Signup, name: 'Signup' },
     { path: '/login', component: Login, name: 'Login' },
-    { path: '/categories', component: CategoryCreate, name: 'CategoryCreate', meta: { requiresAuth: true } }
+    { path: '/categories', component: CategoryCreate, name: 'CategoryCreate', meta: { requiresAuth: true } },
+    { path: '/month_detail/:date_number', component: MonthDetail, name: 'MonthDetail', meta: { requiresAuth: true } },
   ]
 });
 
