60303075e6b5f4cb96a873dc7907b26d04521ef2
tetsu0804 <banginhoodyonezawa@gmail.com>
2021-11-01 21:08:57 +0900

    ShoplistDetail.vue で updaeShoplist と　deleteShoplist追加

--- app/javascript/src/molcules/ShoplistDetail.vue
+++ app/javascript/src/molcules/ShoplistDetail.vue
@@ -17,8 +17,8 @@
           <td>{{ shoplist.list_name }}</td>
           <td>{{ shoplist.price | priceYen }}</td>
           <td>{{ userSearch(shoplist.user_id) }}</td>
-          <td>x</td>
-          <td>x</td>
+          <td><div class="clickIcon" v-if="userIdMatchedShoplist(shoplist.user_id)" @click="updateShoplist(shoplist.id)"></div></td>
+          <td><div class="clickIcon" v-if="userIdMatchedShoplist(shoplist.user_id)" @click="deleteShoplist(shoplist)"></div></td>
         </tr>
       </tbody>
     </table>
@@ -28,6 +28,9 @@
 <script>
   export default {
     props: {
+      userLoggedIn: {
+        type: Object
+      },
       pageSplit: {
         type: Function
       },
@@ -49,6 +52,11 @@
         return (user_id) => {
           return this.userSearchId(user_id).first_name
         }
+      },
+      userIdMatchedShoplist() {
+        return (user_id) => {
+          return user_id === this.userLoggedIn.user.id ? true : false
+        }
       }
     },
     filters: {
@@ -62,6 +70,27 @@
       },
       priceYen(value) {
         return !!value ? value + '円' : ''
+      }
+    },
+    methods: {
+      updateShoplist(value) {
+        this.$emit('shoplistUpdate', value)
+      },
+      deleteShoplist(shoplist) {
+        this.axios.delete(`/api/v1/shop_list/${shoplist.id}`)
+        .then(response => {
+          this.$store.dispatch('fetchDeleteShopList', shoplist.id);
+          const success_format = { message: '', status: '' }
+          success_format.message = `${shoplist.list_name} を削除しました。`
+          success_format.status = response.status
+          this.$emit('shoplistStatus', success_format)
+        })
+        .catch(error => {
+          const error_format = { message: '', status: ''}
+          error_format.message = '削除失敗しました。'
+          error_format.status = !!error.response && !!error.response.status ? error.response.status : 400
+          this.$emit('shoplistStatus', error_format)
+        })
       }
     }
   }
@@ -86,5 +115,19 @@
     text-align: center;
     border-bottom: 1px solid rgba(0, 0, 0, 0.5);
     height: 30px;
+  }
+  .clickIcon {
+    width: 1rem;
+    height: 1rem;
+    margin: 0 auto;
+    border-radius: 50%;
+    background-color: rgb(255, 0, 0);
+    cursor: pointer;
+    border: 1px solid rgb(0, 0, 0);
+    box-shadow: 0 10px 15px 0 rgba(0, 0, 0, .5);
+  }
+  .clickIcon:hover {
+    background-color: rgba(255, 0, 0, 0.5);
+    border: 1px solid rgba(0, 0, 0, 0.1);
   }
 </style>
