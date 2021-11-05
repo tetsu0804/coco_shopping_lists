27aaad50fe98bbe75b40506ac75bb34d90ce6628
tetsu0804 <banginhoodyonezawa@gmail.com>
2021-11-05 22:50:23 +0900

    MainDisplay.vue で レイアウト レスポンシブ 追加

--- app/javascript/src/molcules/MainDisplay.vue
+++ app/javascript/src/molcules/MainDisplay.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="main-display-container">
-    <h1>{{ mainDisplay(dateNum).month_display}}</h1>
+    <div class="main-display-title">
+      <h1>{{ mainDisplay(dateNum).month_display}}</h1>
+    </div>
     <div class="arrow-price">
       <div class="arrow-price-split arrows arrow-left">
         <i v-if="leftArrow" class="far fa-arrow-alt-circle-left fa-3x" @click="minusOne"></i>
@@ -13,8 +15,11 @@
       </div>
       <div class="clear-float"></div>
     </div>
-    <router-link :to="{ name: 'MonthDetail', params: { date_number: dateNum }}" class="last-shopping">最後に購入した商品: {{ mainDisplay(dateNum).last_shoplist.list_name}}</router-link>
-    <div class="clear-right"></div>
+
+    <div class="main-link">
+      <router-link :to="{ name: 'MonthDetail', params: { date_number: dateNum }}" class="last-shopping">最後に購入した商品: {{ mainDisplay(dateNum).last_shoplist.list_name}}</router-link>
+      <div class="clear-right"></div>
+    </div>
   </div>
 </template>
 
@@ -61,19 +66,36 @@
 <style scoped>
   .main-display-container {
     width: 100%;
+    height: 100%;
     margin: 0;
     padding: 0;
+    background-color: rgb(89, 149, 210);
   }
-
-  .main-display-container h1 {
+  .main-display-title {
+    width:100%;
+    background-color: rgb(149, 57, 129);
+    height: 40%;
+    margin: 0;
+    padding: 0;
+    display: table;
+  }
+  .main-display-container .main-display-title h1 {
     text-align: center;
+    margin-top: 0;
+    margin-bottom: 0;
+    /* padding-top: 10%; */
+    /* background-color: rgb(16, 83, 56); */
+    /* height: 20%; */
+    display: table-cell;
+    vertical-align: middle;
   }
 
   .arrow-price {
     width: 100%;
-    height: 200px;
+    height: 50%;
     margin: 0;
     padding: 0;
+    background-color: rgb(19, 193, 181);
   }
   .arrow-price-split {
     float: left;
@@ -100,21 +122,40 @@
   .arrow-right {
     text-align: left;
   }
+  .main-link {
+    width: 100%;
+    height: 10%;
+    margin: 0;
+    padding: 0;
+    background-color: rgba(200, 222, 138, 0.42);
+  }
   .last-shopping {
     float: right;
     text-decoration: none;
     color: rgb(0, 0, 0);
+    background-color:rgb(64, 9, 136);
+    font-size: 1rem;
   }
   .last-shopping:hover {
     color: rgba(0, 0, 0, 0.5);
   }
   .clear-right {
     clear: right;
+    margin: 0;
+    padding: 0;
   }
   .far {
     cursor: pointer;
   }
   .far:hover {
     color: rgba(0, 0, 0, 0.5);
+  }
+  @media screen and (max-width: 480px) {
+    .last-shopping {
+      text-decoration: none;
+      color: rgb(0, 0, 0);
+      background-color:rgb(64, 9, 136);
+      font-size: 0.9rem;
+    }
   }
 </style>
