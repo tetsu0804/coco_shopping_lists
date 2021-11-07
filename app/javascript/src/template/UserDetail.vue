dac8d8352cbfa1d3218bd60ec96da46bfae10968
tetsu0804 <banginhoodyonezawa@gmail.com>
2021-11-07 16:15:07 +0900

    UserDetail.vue templateと import HeaderLink と import UserShow 初期状態追加

---
+++ app/javascript/src/template/UserDetail.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="user-detail-container">
+    <div class="user-detail-sub-container">
+      <div class="user-detail-head-container">
+        <header-link
+          :userLoggedIn="userLoggedIn"
+          @logoutStatus="flash = $event"
+        ></header-link>
+      </div>
+      <div class="user-detail-show-container">
+        <user-show
+        ></user-show>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import HeaderLink from './HeaderLink.vue'
+import UserShow from '../molcules/UserShow.vue'
+import { mapGetters } from 'vuex'
+  export default {
+    components: {
+      HeaderLink,
+      UserShow
+    },
+    data() {
+      return {
+        flash: ''
+      }
+    },
+    computed: mapGetters(['userLoggedIn'])
+  }
+</script>
+
+<style scoped>
+  .user-detail-container {
+    width: 100%;
+    height: 100vh;
+    background-color: rgba(0, 0, 0, 0.06);
+  }
+  .user-detail-sub-container {
+    width: 90%;
+    height: 100%;
+    margin: 0 auto;
+  }
+  .user-detail-head-container {
+    width: 80%;
+    height: 10%;
+    margin: 0 auto;
+  }
+</style>
