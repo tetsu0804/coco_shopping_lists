8f6095b103adb9edaaaea8ef9ee1ff708252f834
tetsu0804 <banginhoodyonezawa@gmail.com>
2021-11-14 21:09:41 +0900

    chart.js userIdSplistShopListsと userIdAndOtherShopList 修正

--- app/javascript/src/modules/chart.js
+++ app/javascript/src/modules/chart.js
@@ -9,53 +9,55 @@ export default {
   userIdSplitShopLists(shoplists, user_id) {
     let all_users_ids = [], id_split_shoplists = [], mySetArray, user_id_index, splice_id_split_shoplists, sortUserShoplists
     let mySet = new Set()
-    shoplists.forEach( (shoplist, index) => {
-      all_users_ids.push(shoplist.user_id)
-      mySet.add(shoplist.user_id)
-    });
-    mySetArray = [...mySet]
-
-    // console.log(mySet)
-    // console.log(mySetArray)
-    //{ 0: {user_id: id, shoplists: n}}
-    for (let i = 0; i < mySetArray.length; i++) {
-      id_split_shoplists[i] = {}
-      id_split_shoplists[i].user_id = mySetArray[i]
-      id_split_shoplists[i].shoplists = 0
-      for(let n = 0; n < all_users_ids.length; n++) {
-        if (mySetArray[i] === all_users_ids[n]) {
-          id_split_shoplists[i].shoplists += 1
+    if (shoplists.length > 0) {
+      shoplists.forEach( (shoplist, index) => {
+        all_users_ids.push(shoplist.user_id)
+        mySet.add(shoplist.user_id)
+      });
+      mySetArray = [...mySet]
+      for (let i = 0; i < mySetArray.length; i++) {
+        id_split_shoplists[i] = {}
+        id_split_shoplists[i].user_id = mySetArray[i]
+        id_split_shoplists[i].shoplists = 0
+        for(let n = 0; n < all_users_ids.length; n++) {
+          if (mySetArray[i] === all_users_ids[n]) {
+            id_split_shoplists[i].shoplists += 1
+          }
         }
       }
+      if (mySetArray.includes(user_id)) {
+        user_id_index = id_split_shoplists.findIndex((split) => split.user_id === user_id);
+        splice_id_split_shoplists = id_split_shoplists.splice(user_id_index, 1);
+      } else {
+        splice_id_split_shoplists = [ {user_id: user_id, shoplists: 0} ]
+      }
+      sortUserShoplists = this.ascendingUserShoplist(id_split_shoplists);
+      sortUserShoplists.unshift(splice_id_split_shoplists[0]);
+    } else {
+      sortUserShoplists = [
+        { user_id: user_id, shoplists: 0},
+        { user_id: '', shoplists: ''}
+      ]
     }
-    user_id_index = id_split_shoplists.findIndex((split) => split.user_id === user_id);
-    //console.log(user_id_index)
-    //console.log(id_split_shoplists)
-    splice_id_split_shoplists = id_split_shoplists.splice(user_id_index, 1);
-    //console.log(splice_id_split_shoplists)
-    //console.log(id_split_shoplists)
 
-    //console.log(this.ascendingUserShoplist(id_split_shoplists))
-    sortUserShoplists = this.ascendingUserShoplist(id_split_shoplists)
-    sortUserShoplists.unshift(splice_id_split_shoplists[0])
     return sortUserShoplists
-    //console.log(sortUserShoplists)
-    // console.log(splice_id_split_shoplists[0])
-    // console.log(sortUserShoplists)
-    // console.log(typeof sortUserShoplists)
-    //console.log(sortUserShoplists.unshift(splice_id_split_shoplists[0]))
-    //console.log(sortUserShoplists.unshift(splice_id_split_shoplists[0]))
-    //id_split_shoplists.unshift(splice_id_split_shoplists[0]);
-
-    //console.log(id_split_shoplists)
-
-
-
-    //}
-    // all_users_ids=[1,1,2,3,2,1,1,3,2,1,3]
-    // all_users_ids.forEach((id, index , self) => {
-    //
-    // });
+  },
+  userIdAndOtherShopList(userIdSplitShopLists) {
+    let other_shplists_plus = 0, other_format = {user_id: -1, shoplists: 0}, initialize_format = [], user_shoplists
+    if (userIdSplitShopLists.length > 0) {
+      user_shoplists = userIdSplitShopLists.splice(0, 1)
+      userIdSplitShopLists.forEach(userShoplist => {
+        other_format.shoplists += userShoplist.shoplists
+      });
+      initialize_format.push(user_shoplists[0]);
+      initialize_format.push(other_format);
+    } else {
+      initialize_format = [
+        {user_id: '', shoplists: ''},
+        {user_id: '', shoplists: ''}
+      ]
+    }
+    return initialize_format
   },
   ascendingUserShoplist(shoplists) {
     return shoplists.sort(
@@ -91,5 +93,8 @@ export default {
       parcent_propaty_format.rgb = 'background-color: rgba(0, 0, 0, 0.5);'
     }
     return parcent_propaty_format
+  },
+  usersAndOthresSplit() {
+
   }
 }
